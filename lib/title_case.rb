6ac8a3ebf1e02aafb7f9164c9c2c4ac490c03752
Evans Musomi <evans@habahaba.co>
2016-10-05 10:30:55 +0300

    Adds tests and code to make them pass

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,3 +1,18 @@
 class TitleCase
-
+  # Correctly formats input string to title case as specified
+  def self.format_title(title)
+    # Lower case input
+    title = title.downcase
+    # Split sentence to wordsa
+    title = title.split(' ')
+    # Capitalise word if is first, last or not in exempt list
+    exempt_list = ['a', 'an', 'and', 'the', 'but', 'or', 'for', 'nor', 'on', 'at', 'to', 'from', 'by']
+    (0...title.length).each do |index|
+      if index == 0 || index == title.length - 1 || !exempt_list.include?(title[index])
+        title[index] = title[index].capitalize
+      end
+    end
+    # Re-join words and return
+    title.join(' ')
+  end
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -1,3 +1,23 @@
 # Include spec-related files
 require 'spec_helper'
 require 'title_case'
+
+describe TitleCase do
+  describe ".format_title" do
+    it "returns correctly formatted title" do
+      expect(TitleCase.format_title("I'm a little tea pot")).to eq "I'm a Little Tea Pot"
+    end
+
+    it "returns correctly formatted title for edge case" do
+      expect(TitleCase.format_title("sHoRt AnD sToUt")).to eq "Short and Stout"
+    end
+
+    it "returns correctly formatted title when one word given" do
+      expect(TitleCase.format_title("hEllO")).to eq "Hello"
+    end
+
+    it "returns empty string when empty input provided" do
+      expect(TitleCase.format_title("")).to eq ""
+    end
+  end
+end
